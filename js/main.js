7dd899805f4869204228dcb6a7a352f170c899f9
countchrisdo <crburwell@yahoo.com>
2021-06-08 00:46:55 -0400

    Reworking gameboard and array checking + css style changes

--- js/main.js
+++ js/main.js
@@ -1,43 +1,54 @@
 /*----- constants -----*/
+const colors = {
+    '1' : "#ffc2e2", //red
+    '-1' : "#bbbaff", //black
+    '0' : "#f7fcff" //white
+}
 const TOTALTURNS = 42;
-const testVar = 0;
+
+CLEARBOARD = [
+    [0, 0, 0, 0, 0, 0],  // Column 0
+    [0, 0, 0, 0, 0, 0],  // Column 1
+    [0, 0, 0, 0, 0, 0],  // Column 2
+    [0, 0, 0, 0, 0, 0],  // Column 3
+    [0, 0, 0, 0, 0, 0],  // Column 4
+    [0, 0, 0, 0, 0, 0],  // Column 5
+    [0, 0, 0, 0, 0, 0],  // Column 6
+];
 
 /*----- app's state (variables) -----*/
-let gameStatus = "inGame"
-let playerTurn = "Black"; //Black or Red
+let testVar = 1;
+let board;
 let turnsTaken = 0;
-
+let playerTurn; // 1, -1
+let winner; // null, 1, -1, "T"
+ 
 /*----- cached element references -----*/
+const colBtns = [...document.querySelectorAll("#column-buttons > div")];
 const replayEl = document.getElementById("replay");
-const slotsEl = document.getElementsByClassName("slot");
-const msgEl = document.getElementById("msg");
+const headerEl = document.getElementById("header");
 
-/*----- event listeners -----*/
-document.querySelector("td").addEventListener("click", handleSlotClick);
+ /*----- event listeners -----*/
+document.getElementById("column-buttons").addEventListener("click", handleMove);
 document.getElementById("replay").addEventListener("click", init);
-
-/*----- functions -----*/
+// /*----- functions -----*/
 init();
 
 function init() {
-    //rng to find 1st player 
-    const randomIdx = Math.floor(Math.random() * 2);
-    if(randomIdx == 0){playerTurn = "Black"} else {playerTurn = "Red"}
+     //rng to find 1st player 
+     const randomIdx = Math.floor(Math.random() * 2);
+     if(randomIdx == 0){playerTurn = 1} else {playerTurn = -1}
     
-    board = [
-        [0, 0, 0, 0, 0, 0],  // Column 0
-        [0, 0, 0, 0, 0, 0],  // Column 1
-        [0, 0, 0, 0, 0, 0],  // Column 2
-        [0, 0, 0, 0, 0, 0],  // Column 3
-        [0, 0, 0, 0, 0, 0],  // Column 4
-        [0, 0, 0, 0, 0, 0],  // Column 5
-        [0, 0, 0, 0, 0, 0],  // Column 6
-      ];
-      console.log("Game started/restarted");
-      console.log("Variables Reset");
-      renderMessage()
-      render();
+    turnsTaken = 0;
+    board = CLEARBOARD;
+    playerTurn = 1;
+    winner = null;
+
+    console.log("Game started/restarted");
+    console.log("Variables Reset");
+    render();
 }
+
 function render() {
     // itterate over each column in  board
     board.forEach((column, columnidx) => {
@@ -46,92 +57,54 @@ function render() {
             // find specific cell using columnidx and cellidx
             let div = document.getElementById(`c${columnidx}r${cellidx}`);
             div.style.backgroundColor = colors[cell];
-        })
+        });
+        colBtns[columnidx].style.visibility = column.includes(0) ? "visible" : "hidden";
     });
-    
-    //only show replay button at the end of the game
-    if (gameStatus == "inGame") {
-        replayEl.style.visibility = "hidden"
-    } else {
-        replayEl.style.visibility = "visible"
-    };
+    //render message
+    if(playerTurn == 1){
+    headerEl.innerHTML = `Player ${playerTurn}'s turn! <br> Good Luck!`
+    } else {`Player 2's turn! <br> Change Later`}
+
     console.log("Render has run / Page Updated");
+
+    // if (gameStatus == "winB") {
+    //     msgEl.innerText = "Player Black wins"
+    // } else if (gameStatus == "winR") {
+    //     msgEl.innerText = "Player White wins"
+    // } else if (gameStatus == "winT") {
+    //     msgEl.innerText = "TIE GAME"
+    // } else {
+    //     //state player's turn
+    //     msgEl.innerHTML = `Player ${playerTurn}'s turn! <br> Good Luck!`
+    // }
 }
 
-// function renderSlots() {
-//     slotsEl.forEach(function (sqr) {
-//         //const tile = sqr.innerText;
-//         if (sqr.value == "X") {
-//             sqr.disabled;
-//             sqr.className = "X";
-//         } else if (sqr.value == "O") {
-//             sqr.disabled;
-//             sqr.className = "O";
-//         } else {
-//             sqr.className = "empty"
-//         }
-//     })
-// }
+//win logic (not done)
+function getWinner(colIdx, rowIdx) {
 
-function renderMessage() {
-    //win message
-    if (gameStatus == "winB") {
-        msgEl.innerText = "Player Black wins"
-    } else if (gameStatus == "winR") {
-        msgEl.innerText = "Player White wins"
-    } else if (gameStatus == "winT") {
-        msgEl.innerText = "TIE GAME"
-    } else {
-        //state player's turn
-        msgEl.innerHTML = `Player ${playerTurn}'s turn! <br> Good Luck!`
-    }
 }
 
-function handleSlotClick(evt) {
-    //if game isn't running do nothing
-    getGameStatus();
-    if(gameStatus !== "inGame") {
-        console.log("Game not running, click failed")
-        return false;}
+function handleMove(evt) {
+    //   //if game isn't running do nothing
+    //   getGameStatus();
+    //   if(gameStatus !== "inGame") {
+    //       console.log("Game not running, click failed")
+    //       return false;}
+      
     
-    //if Slot is full, do nothing. if empty, log X/O
-    if (evt.target.className !== "") {
-        console.log("Slot is full, try again");
-        return;
-    } else {
-        evt.target.value = `${playerTurn}`;
-    };
-
-    playerTurn == "Black" ? playerTurn = "Red" : playerTurn = "Black";
-
-    getGameStatus();
+    // update all impacted state
+    const colIdx = colBtns.indexOf(evt.target);
     
-    turnsTaken++;
+    if (colIdx === -1 || winner) return;
+    const colArr = board[colIdx];
+    const rowIdx = colArr.indexOf(0);
+    
+    if (rowIdx === -1) return;
+    colArr[rowIdx] = playerTurn;
+    playerTurn = playerTurn * -1;
 
+    winner = getWinner(colIdx, rowIdx);
     render();
-    GameLog();
-}
 
-//Checking the state of the game
-function getGameStatus() {
-    if (turnsTaken == TOTALTURNS) {
-        console.log("Game Status after Game Status Check: " + gameStatus);
-        return "winT"
-    }
-    //Checking Winner 
-    if (turnsTaken >= 4) {
 
-       
-        //tie game
-        if (testVar) {
-            return gameStatus = "winT"
-        };
-        return gameStatus = "inGame";
-    } 
-}
-gameStatus = getGameStatus();
-
-if (turnsTaken >= "42") {
-    getGameStatus();
-    render();
 }
